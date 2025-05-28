11aa4758c4f85bef4f0c3bf9f6fd4b066d361106
om-sonwane15 <om7sonwane@gmail.com>
2025-05-28 13:13:21 +0530

    updated file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Login from "./Pages/Login";
 import Home from "./Pages/Home";
 import ChangePassword from "./Pages/ChangePassword";
 import ForgotPassword from "./Pages/ForgotPassword";
-
-return (
-  <Routes>
-    <Route path="/dashboard" element={<Home />} />
-    <Route path="/register" element={<Register />} />
-    <Route path="/" element={<Login />} />
-    <Route path="/ChangePassword" element={<ChangePassword />} />
-    <Route path="/forgot" element={<ForgotPassword />} />
-    {/* <Route path="/reset" element={<ResetPassword />} /> */}
-  </Routes>
-);
+function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/ChangePassword" element={<ChangePassword />} />
+        <Route path="/forgot" element={<ForgotPassword />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 export default App;
---
+++ frontend/src/Pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const Login = ()=>{
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      <div className="flex-1 ml-64 p-10">
+        <h2 className="text-3xl font-bold text-gray-700">Login Page Uploading soon...</h2>
+      </div>
+    </div>
+  );
+};
+export default Login;
