cc725d80dd97224dd8df02824cfc03e2e1a21785
om-sonwane15 <om7sonwane@gmail.com>
2025-05-28 12:28:02 +0530

    Updated the structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,17 @@
-import { useState } from 'react';
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Home from "./Pages/Home";
+import ChangePassword from "./Pages/ChangePassword";
+import ForgotPassword from "./Pages/ForgotPassword";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+return (
+  <Routes>
+    <Route path="/dashboard" element={<Home />} />
+    <Route path="/register" element={<Register />} />
+    <Route path="/" element={<Login />} />
+    <Route path="/ChangePassword" element={<ChangePassword />} />
+    <Route path="/forgot" element={<ForgotPassword />} />
+    {/* <Route path="/reset" element={<ResetPassword />} /> */}
+  </Routes>
+);
+export default App;
