01a2a0e6bd05052fd0dd9ed5a003b0807a8915f0
Akshat4504 <136491069+Akshat4504@users.noreply.github.com>
2025-05-30 15:10:15 +0530

    deploy

--- backend/main.js
+++ backend/main.js
@@ -39,7 +39,7 @@ app.use((req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
 });
