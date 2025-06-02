0838a0a74b9cbed6f81f3afbeb21b9557be06b48
om-sonwane15 <om7sonwane@gmail.com>
2025-06-02 19:20:46 +0530

    Changes

--- frontend/src/Pages/AdminDashboard.jsx
+++ frontend/src/Pages/AdminDashboard.jsx
@@ -346,13 +346,11 @@ const AdminDashboard = () => {
                   <div>
                     <label className="block text-sm font-medium text-gray-700 mb-2">Date *
                     </label>
-                    <input
-                      type="date"
-                      name="date"
-                      value={form.date}
-                      onChange={handleInputChange}
-                      className="w-full px-4 py-3 border border-gray-300 rounded-xl focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all"
-                      required />
+                    <input type="date"name="date"value={form.date}
+                    min={new Date().toISOString().split("T")[0]}  
+                    onChange={handleInputChange}
+                    className="w-full px-4 py-3 border border-gray-300 rounded-xl focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all"
+                    required/>
                   </div>
                   <div>
                     <label className="block text-sm font-medium text-gray-700 mb-2">Type *
